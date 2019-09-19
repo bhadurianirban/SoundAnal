c525799547f9674b3ead0d826ceacb3241f1a552
Anirban Bhaduri <bhaduri.anirban@gmail.com>
2019-09-19 16:57:01 +0530

    Jibon Jantrana

--- AudioParse.py
+++ AudioParse.py
@@ -1,30 +1,43 @@
-import librosa
-import librosa.display
-import numpy as np
-import matplotlib.pyplot as plt
 import os
+import re
+import librosa
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
-def getlabels(audio_file_path):
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(audio_file_path):
-        for file in f:
-            if '.wav' in file:
-                files.append(file)
-    return files
+class AudioParse:
 
+    def __init__(self, audio_file_root):
+        self.audio_file_root = audio_file_root
+        self.audio_file_emotion = []
+        self.audio_file_feature = []
+        self.audio_file_label = []
 
-audio_file_path = '/home/dgrfi/Documents/WavData/All/'
-audio_files = getlabels(audio_file_path)
-print(audio_files)
+    def getlabels(self):
+        files = []
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(self.audio_file_root):
+            for file in f:
+                if '.wav' in file:
+                    files.append(file)
 
-#
-# getLabels(path)
-# for f in files:
-#    print(f)
-# data, sampling_rate = librosa.load('/home/dgrfi/Documents/WavData/All/OAF_back_angry.wav')
-# plt.figure(figsize=(12, 4))
-# librosa.display.waveplot(data, sr=22050)
-# mfccs = librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40)
-# print(mfccs.shape)
+        for file_name in files:
+            file_name_split = re.split('[_.]', file_name)
+            self.audio_file_emotion.append((file_name, file_name_split[2]))
+        # print(sound_file_label)
+
+    def derive_features(self):
+        for file_name, emotion in self.audio_file_emotion:
+            audio_file_full_path = self.audio_file_root + file_name
+            print("Extracting feature for " + audio_file_full_path)
+            try:
+                # here kaiser_fast is a technique used for faster extraction
+                X, sample_rate = librosa.load(audio_file_full_path, res_type='kaiser_fast')
+                # we extract mfcc feature from data
+                mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            except IOError as e:
+                print("Error encountered while parsing file: ", file_name)
+
+            self.audio_file_feature.append(mfccs)
+            self.audio_file_label.append(emotion)
+        print(self.audio_file_label)
---
+++ SoundAnalMain.py
@@ -0,0 +1,16 @@
+from AudioParse import AudioParse
+
+audio_file_path = '/home/dgrfi/Documents/WavData/All/'
+audio_parse = AudioParse(audio_file_path)
+audio_parse.getlabels()
+audio_parse.derive_features()
+print("Done")
+#
+# getLabels(path)
+# for f in files:
+#    print(f)
+# data, sampling_rate = librosa.load('/home/dgrfi/Documents/WavData/All/OAF_back_angry.wav')
+# plt.figure(figsize=(12, 4))
+# librosa.display.waveplot(data, sr=22050)
+# mfccs = librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40)
+# print(mfccs.shape)
