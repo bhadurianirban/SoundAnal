981a2ed73aa1181bd4ce1e8e4397061bfb3c2d6a
Anirban Bhaduri <bhaduri.anirban@gmail.com>
2019-09-24 15:17:57 +0530

    Jibon Jantrana

--- AudioParse.py
+++ AudioParse.py
@@ -4,6 +4,8 @@ import librosa
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
 
 
 class AudioParse:
@@ -30,24 +32,45 @@ class AudioParse:
     def derive_features(self):
         for file_name, emotion in self.audio_file_emotion:
             audio_file_full_path = self.audio_file_root + file_name
-            # print("Extracting feature for " + audio_file_full_path)
-            # try:
-            #     # here kaiser_fast is a technique used for faster extraction
-            #     X, sample_rate = librosa.load(audio_file_full_path, res_type='kaiser_fast')
-            #     # we extract mfcc feature from data
-            #     mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-            # except IOError as e:
-            #     print("Error encountered while parsing file: ", file_name)
+            print("Extracting feature for " + audio_file_full_path)
+            try:
+                # here kaiser_fast is a technique used for faster extraction
+                X, sample_rate = librosa.load(audio_file_full_path, res_type='kaiser_fast')
+                # we extract mfcc feature from data
+                mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            except IOError as e:
+                print("Error encountered while parsing file: ", file_name)
 
-            # self.audio_file_feature.append(mfccs)
+            self.audio_file_feature.append(mfccs)
             self.audio_file_label.append(emotion)
         # print(self.audio_file_label)
 
     def label_encoding(self):
         np_label = np.array(self.audio_file_label)
-        # np_features = np.array(self.audio_file_feature)
+        np_features = np.array(self.audio_file_feature)
 
         lb = LabelEncoder()
         # np_label = np_utils.to_categorical(lb.fit_transform(np_label))
-        np_label_encoded = lb.fit_transform(self.audio_file_label)
-        print(np_label_encoded.shape)
+        np_label_encoded = lb.fit_transform(np_label)
+        y = np_utils.to_categorical(np_label_encoded)
+        print(list(np_label_encoded))
+
+        num_labels = y.shape[1]
+        filter_size = 2
+
+        # build model
+        model = Sequential()
+
+        model.add(Dense(256, input_shape=(40,)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+
+        model.add(Dense(256))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+
+        model.add(Dense(num_labels))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+        model.fit(np_features, y, batch_size=32, epochs=5, validation_split=0.1)
